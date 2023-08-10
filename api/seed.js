162afea5af0a3a76dfb77db15229a5696c29acb1
Bagas Hizbullah <work.bitsy@simplelogin.com>
2023-08-10 16:48:20 +0700

    feat(deploy): publish docker image
    - remove unnecessary .env
    - remove seed confirmation

--- api/seed.js
+++ api/seed.js
@@ -1,21 +1,7 @@
-const readline = require('readline');
 const bcrypt = require('bcrypt');
 const prisma = require('./provider/client');
-const rl = readline.createInterface({
-    input: process.stdin, output: process.stdout,
-});
 
 async function seedAdmins() {
-    const confirmSeed = await new Promise((resolve) => {
-        rl.question('Do you want to seed admin users? (y/n): ', (answer) => {
-            resolve(answer.toLowerCase() === 'y');
-        });
-    });
-    if (!confirmSeed) {
-        console.log('Admin seeding cancelled.');
-        rl.close();
-        return;
-    }
     const adminData = [{
         email: 'dev@hology.ub.ac.id', name: 'Rafa', role: 'GOD', password: 'rafaturu'
     }, {
